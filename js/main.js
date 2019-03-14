8a5e13f134d7081c718ddde3670c96bee4fa21b3
Pal Pandian <impalpandian@gmail.com>
2019-03-14 16:32:09 +0530

    new template on sep branch

--- js/main.js
+++ js/main.js
@@ -1,256 +1,123 @@
+(function ($) {
+  "use strict";
 
-"user strict";
-/*
-====================== components =======================    
-    1.Video Model 
-    2.Timer
-    3.Smooth scroll
-    4.Background-paralax
-    5.Header sticky
-    6.Mail function
-    7.Loader JS
-*/
-
-
-/*-----------------------1 Video modal JS ------------------*/
-$(document).ready(function(){
-   var $button = $('.open-video-modal'),
-       $overlay = $('.video-overlay'),
-       $modal = $('.video-modal'),
-       $player = $('#player'),
-       $close = $('#video_modal_close'),
-       player,
-       getVideoID = '668nUCeBHyY';
-   
-   //setup video
-   var onYouTubePlayerAPIReady = function() {
-      player = new YT.Player('player', {
-        /*height: '390',
-        width: '640',*/
-        videoId: getVideoID,
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
+  // Preloader (if the #preloader div exists)
+  $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
       });
-   }
-   
-   // Open modal after click
-   $button.click(function(e){
-      e.preventDefault();
-      if (!player) onYouTubePlayerAPIReady();
-      $overlay.attr('aria-hidden', 'false');
-      $modal.attr('tabindex', '0');
-      $button.hide();
-   });
-   
-   // Close modal by btn click/hit
-  $close.click(function(e){
-     e.preventDefault();
-     closeSection();
+    }
   });
-   
-   // autoplay video
-   function onPlayerReady(event) {
-      event.target.playVideo();
-   }
 
-   // Video ends
-   function onPlayerStateChange(event) {        
-      if(event.data === 0) {
-         closeSection();
-      }
-   }
-   
-   // Stop video
-   function resetPlayer() {
-      player.pauseVideo();
-   };
-   
-   //close modal
-   function closeSection(){
-      $button.show();
-      $overlay.attr('aria-hidden', 'true');
-      $modal.attr('tabindex', '-1');
-      resetPlayer();
-   }
-   
-   //check where mouse is clicked
-   $overlay.mousedown(function(e) {
-      var clicked = $(e.target); // get the element clicked
-      if (clicked.is('.video-modal') || clicked.parents().is('video-modal')) {
-         return; // click happened within the dialog, do nothing here
-      } else { // click was outside the dialog, so close it
-         closeSection();
-      }
-   });
-   
-   $(document).keydown(function(e) {
-      // ESCAPE key pressed
-      if (e.keyCode === 27) {
-         closeSection();
-      }
-   });
-});
-// video modal JS END
-/*-------------------------- 2.Timer --------------------------*/
-function makeTimer() {
-    var endTime = new Date("September 10, 2018 18:00:00 PDT"); // Change timer date form here...!        
-    var endTime = (Date.parse(endTime)) / 1000;
-    var now = new Date();
-    var now = (Date.parse(now) / 1000);
-    var timeLeft = endTime - now;
-    var days = Math.floor(timeLeft / 86400);
-    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
-    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-    if (days < "10") {
-        days = "0" + days;
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
     }
-    if (hours < "10") {
-        hours = "0" + hours;
-    }
-    if (minutes < "10") {
-        minutes = "0" + minutes;
-    }
-    if (seconds < "10") {
-        seconds = "0" + seconds;
-    }
-    $(".days").html(days + "<span>Days</span>");
-    $(".hours").html(hours + "<span>Hours</span>");
-    $(".minutes").html(minutes + "<span>Minutes</span>");
-    $(".seconds").html(seconds + "<span>Seconds</span>");
-}
-setInterval(function() {
-    makeTimer();
-}, 1000);
-var NavBar = $('.navbar ');
-/*---------Timer-end-----------*/
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
 
-/*------------------------------------- 3.Smooth scroll --------------------------------*/
-// redirect-btn - this class for smooth scroll of other button instead of header menu ..! 
-$(document).ready(function() {
-    $('.redirect-btn, .navbar a, .links a').on('click', function(event) {
-        event.preventDefault();
-        var target = $(this.hash);
-        if ($(window).width() < 992) {
-            $('body,html').animate({
-                'scrollTop': target.offset().top - 68
-            }, 400);
-        } else {
-            $('body,html').animate({
-                'scrollTop': target.offset().top - 80
-            }, 400);
+  // Initiate the wowjs animation library
+  new WOW().init();
+
+  // Header scroll class
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Smooth scroll for the navigation and links with .scrollto classes
+  $('.main-nav a, .mobile-nav a, .scrollto').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if ($('#header').length) {
+          top_space = $('#header').outerHeight();
+
+          if (! $('#header').hasClass('header-scrolled')) {
+            top_space = top_space - 20;
+          }
         }
-        $('.navbar-collapse').removeClass('in');
-        $('.navbar-toggle').addClass('collapsed');
-    });
-    /*-------- Smooth scroll-end -----------*/
 
-    /*----------------------------- 4.Background-parallax ---------------------------*/
-    $(window).scroll(function(e) {
-        parallax();
-    });
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
 
-    function parallax() {
-        var scrolled = $(window).scrollTop();
-        $('.parallax').css('background-position-y', -(scrolled * 0.2) + 'px');
-    } /*--------- Background-paralax-end ----------*/
-
-    /*------------------------------- 5.Header sticky -------------------------*/
-    // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var navbarHeight = NavBar.outerHeight();
-    $(window).scroll(function(event) {
-        didScroll = true;
-    });
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
+        if ($(this).parents('.main-nav, .mobile-nav').length) {
+          $('.main-nav .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
         }
-    }, 100);
 
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        if (st + $(window).height() < $(document).height()) {
-            NavBar.addClass('sticky-header');
-            if (st == 0) {
-                NavBar.removeClass('sticky-header');
-            }
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('.mobile-nav-overly').fadeOut();
         }
-        lastScrollTop = st;
+        return false;
+      }
     }
-});
+  });
 
-/*------------ Hader sticky-end  -------*/
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.main-nav, .mobile-nav');
+  var main_nav_height = $('#header').outerHeight();
 
-/*--------------------------------- 6.Mail Fucntion -------------------------------*/
-$(function() {
-    var form = $('#ajax-contact');
-    var formMessages = $('#form-messages');
-    $(form).submit(function(e) {
-        e.preventDefault();
-        var formData = $(form).serialize();
-        $.ajax({
-                type: 'POST',
-                url: $(form).attr('action'),
-                data: formData
-            })
-            .done(function(response) {
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
-                $(formMessages).text(response);
-                $('#first_name').val('');
-                $('#last_name').val('');
-                $('#email').val('');
-                $('#message').val('');
-            })
-            .fail(function(data) {
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.'); /*--------- Contact submission erroe Message ---------------*/
-                }
-            });
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+  
+    nav_sections.each(function() {
+      var top = $(this).offset().top - main_nav_height,
+          bottom = top + $(this).outerHeight();
+  
+      if (cur_pos >= top && cur_pos <= bottom) {
+        main_nav.find('li').removeClass('active');
+        main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
+      }
     });
+  });
 
-    var form1 = $('#subscribe_now');
-    var mailMessages = $('#mail-messages');
-    $(form1).submit(function(e) {
-        e.preventDefault();
-        var formData = $(form1).serialize();
-        $.ajax({
-                type: 'POST',
-                url: $(form1).attr('action'),
-                data: formData
-            })
-            .done(function(response) {
-                $(mailMessages).removeClass('error');
-                $(mailMessages).addClass('success');
-                $(mailMessages).text(response);
-                $('#email').val('');
-            })
-            .fail(function(data) {
-                $(mailMessages).removeClass('success');
-                $(mailMessages).addClass('error');
-                if (data.responseText !== '') {
-                    $(mailMessages).text(data.responseText);
-                } else {
-                    $(mailMessages).text('Oops! An error occured and your message could not be sent.'); /*--------- Contact submission erroe Message ---------------*/
-                }
-            });
+  // jQuery counterUp (used in Whu Us section)
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Porfolio isotope and filter
+  $(window).on('load', function () {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
     });
-});
+    $('#portfolio-flters li').on( 'click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+  
+      portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    });
+  });
 
-/* -------------- Mail function-end ----------*/
-$(document).ready(function($) {
-new WOW().init();
-    /*----------------------- 7.Loader JS -------------------*/
-    $('.site-loader').fadeOut(600);
-});
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
 
-particlesJS("particles-js", {"particles":{"number":{"value":50,"density":{"enable":true,"value_area":1000}},"color":{"value":"#4e68f0"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+})(jQuery);
+
